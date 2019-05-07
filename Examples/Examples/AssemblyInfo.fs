f49a9eea3e842c6db14fd7569f3da5c530bc9d38
czuria1 <czuria1@gmail.com>
2019-05-07 11:22:06 -0400

    initial commit of examples solution

---
+++ Examples/Examples/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace Examples
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Examples")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
