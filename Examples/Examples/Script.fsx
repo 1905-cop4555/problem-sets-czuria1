ddf5e4a35118adb00eccc4f3298963f9ceb5be53
czuria1 <czuria1@gmail.com>
2019-05-08 00:24:09 -0400

    Added script file

---
+++ Examples/Examples/Script.fsx
@@ -0,0 +1,11 @@
+﻿let mk_expon times one = 
+    let rec expon x n = 
+        if n = 0 then one
+        elif n%2 = 0 then expon (times x x) (n/2)
+        else times x (expon x (n-1))
+    expon 
+
+
+let expon1 = mk_expon (*) 1
+
+let expon2 = mk_expon (+) ""
