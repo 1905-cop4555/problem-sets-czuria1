1250f799e1d92805e42347c0ea58a2d16e8901b1
czuria1 <czuria1@gmail.com>
2019-05-09 00:50:10 -0400

    Attempt to rewrite function using match

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -1,4 +1,4 @@
-﻿// Copy example
+﻿(*// Copy example
 let rec fib1 = function 
     | 0 -> 0I
     | 1 -> 1I
@@ -9,8 +9,21 @@ let rec fib2 n =
     match n with
     | 0 -> 0I
     | 1 -> 1I
-    | n -> fib1(n-1) + fib1(n-2)
+    | n -> fib1(n-1) + fib1(n-2)*)
 
-fib1 6
+let mk_expon times one = 
+    let rec expon x n = 
+        if n = 0 then one
+        elif n%2 = 0 then expon (times x x) (n/2)
+        else times x (expon x (n-1))
+    expon
 
-fib2 6
+// Rewrite mk_expon using match
+let mk_expon2 times n = 
+    match n with
+    | 0 = 1
+    | n%2 = 0
+    expon
+
+let expon1 = mk_expon (*) 3
+
