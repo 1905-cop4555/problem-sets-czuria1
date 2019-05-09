071db37f29b0e63fb0dea40f19071de8c183ef6b
czuria1 <czuria1@gmail.com>
2019-05-09 00:31:41 -0400

    First attempt at using match in a function

--- Examples/Examples/Script.fsx
+++ Examples/Examples/Script.fsx
@@ -1,6 +1,16 @@
-﻿let rec fib1 = function 
+﻿// Copy example
+let rec fib1 = function 
+    | 0 -> 0I
+    | 1 -> 1I
+    | n -> fib1(n-1) + fib1(n-2)
+
+// Attempt at match of the same function
+let rec fib2 n =
+    match n with
     | 0 -> 0I
     | 1 -> 1I
     | n -> fib1(n-1) + fib1(n-2)
 
 fib1 6
+
+fib2 6
