b8951c35bdebd8abc68519fc9938e52930d83638
czuria1 <czuria1@gmail.com>
2019-05-09 00:28:34 -0400

    Try fibonacci recursion function

--- Examples/Examples/Script.fsx
+++ Examples/Examples/Script.fsx
@@ -1,11 +1,6 @@
-﻿let mk_expon times one = 
-    let rec expon x n = 
-        if n = 0 then one
-        elif n%2 = 0 then expon (times x x) (n/2)
-        else times x (expon x (n-1))
-    expon 
+﻿let rec fib1 = function 
+    | 0 -> 0I
+    | 1 -> 1I
+    | n -> fib1(n-1) + fib1(n-2)
 
-
-let expon1 = mk_expon (*) 1
-
-let expon2 = mk_expon (+) ""
+fib1 6
