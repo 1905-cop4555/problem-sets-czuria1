7722ee38b6b6d68b22b979a4e7719847d3416d8f
czuria1 <czuria1@gmail.com>
2019-05-11 15:38:18 -0400

    Rearrange files

---
+++ Problem-Sets-1/Problem-Sets-1/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace ProblemSets1
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Problem-Sets-1")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
