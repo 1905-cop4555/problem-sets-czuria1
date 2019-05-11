f67f1d5b71d8d1b115b5951e84d63226be61c759
czuria1 <czuria1@gmail.com>
2019-05-11 14:25:24 -0400

    Attempt at creating a summation recursive function using pattern matching

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -1,6 +1,17 @@
-﻿let rec power (m,n) = 
-    if n = 0
-        then 1.0
-        else m * power (m, n-1);;
+﻿//let rec power (m,n) = 
+//    if n = 0
+//        then 1.0
+//        else m * power (m, n-1);;
 
-power (2.0, 3);;
+//power (2.0, 3);;
+
+let rec sum (n) = 
+    if n = 0
+        then 0
+        else n + sum(n-1);
+
+// Expected output = 6
+sum (3);;
+
+// Expected output = 45
+sum (9);;
