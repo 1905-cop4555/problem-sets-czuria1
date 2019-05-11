68947c245edeb7fde000e119057d1d1c7ff35e74
czuria1 <czuria1@gmail.com>
2019-05-11 14:20:07 -0400

    Tried in class power example but inputting wrong type error

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -1,40 +1,6 @@
-﻿(*// Copy example
-let rec fib1 = function 
-    | 0 -> 0I
-    | 1 -> 1I
-    | n -> fib1(n-1) + fib1(n-2)
+﻿let rec power (m,n) = 
+    if n = 0
+        then 1.0
+        else m * power (m, n-1);;
 
-// Attempt at match of the same function
-let rec fib2 n =
-    match n with
-    | 0 -> 0I
-    | 1 -> 1I
-    | n -> fib1(n-1) + fib1(n-2)*)
-
-//let mk_expon times one = 
-    //let rec expon x n = 
-    //    if n = 0 then one
-    //    elif n%2 = 0 then expon (times x x) (n/2)
-    //    else times x (expon x (n-1))
-    //expon
-
-// Rewrite mk_expon using match
-//let mk_expon2 times n = 
-//    match n with
-//    | 0 = 1
-//    | n%2 = 0
-//    expon
-
-//let expon1 = mk_expon (*) 3
-
-//let rec fib n = 
-//    if n = 0 then -
-//    elif n = 1 then 1
-//    else fib(n-1) + fib(n-2);;
-
-//val fib : int -> int
-
-(17, "abc", true);;
-
-let (a,b,c) = (17, "abc", true);;
-
+power (2.0, 3.0);;
