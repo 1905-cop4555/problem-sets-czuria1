ebb84d330c8abc1550a08a271bdf509beae62a3d
czuria1 <czuria1@gmail.com>
2019-05-12 13:14:06 -0400

    Experimentation with lists

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -1,31 +1,17 @@
-﻿//let rec power (m,n) = 
-//    if n = 0
-//        then 1.0
-//        else m * power (m, n-1);;
+﻿// semicolons are similar to eol
+[1; 2; 3];;
 
-//power (2.0, 3);;
+// another way of writing a list
+[
+1
+2
+3];;
 
-//let rec sum (n) = 
-//    if n = 0
-//        then 0
-//        else n + sum(n-1);
+["hello" ; "world"];;
 
-//// Expected output = 6
-//sum (3);;
+[0..9];;
 
-//// Expected output = 45
-//sum (9);;
+[[1];[6..12];[]];;
 
-let rec mult (m, n) = 
-    if n = 1
-        then m
-        else m + mult(m, n-1);
-
-// Expected output = 5
-mult (5, 1);;
-
-// Expected output = 20
-mult (4, 5);;
-
-// Expected output = -3
-mult (3, -1);;
+// can't mix types in list
+//[[1];["a";"b"];[]]
