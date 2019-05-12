182982357aaf1846d9954ad46eed671d08fbd91f
czuria1 <czuria1@gmail.com>
2019-05-12 13:25:04 -0400

    Experiementation with lists

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -1,17 +1,31 @@
 ﻿// semicolons are similar to eol
-[1; 2; 3];;
+//[1; 2; 3];;
 
 // another way of writing a list
-[
-1
-2
-3];;
+//[
+//1
+//2
+//3];;
 
-["hello" ; "world"];;
+//["hello" ; "world"];;
 
-[0..9];;
+//[0..9];;
 
-[[1];[6..12];[]];;
+//[[1];[6..12];[]];;
 
 // can't mix types in list
 //[[1];["a";"b"];[]]
+
+List.head [1;2;3];;
+List.tail [1;2;3];;
+
+List.map (fun n -> n+n) [1;2;3;4];;
+List.map (fun n -> n*n) [1;2;3;4];;
+List.map (fun n -> n=n+1) [1;2;3;4];;
+
+List.filter (fun x -> x%2 = 0) [1;2;3;4;5];;
+List.filter (fun x -> x%2 > 0) [1;2;3;4;5];;
+
+List.reduce (*) [1..5];;
+List.reduce (+) [1..5];;
+List.reduce (-) [1..5];;
