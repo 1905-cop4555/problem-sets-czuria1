f80ac24096d6923f4b93e44dbcbc5053e566fedf
czuria1 <czuria1@gmail.com>
2019-05-15 20:01:50 -0400

    First test with addition function

--- Problem-Sets-1/Problem-Sets-1/Problem Set 1.fsx
+++ Problem-Sets-1/Problem-Sets-1/Problem Set 1.fsx
@@ -210,6 +210,35 @@ fun x y -> x + " " + y
 fun f -> f (f "cat")
 
 (*
+16. A fraction like 2/3 can be represented in F# as a pair of type int * int. Define infix operators .+ and .* to do 
+    addition and multiplication of fractions:
+      > (1,2) .+ (1,3);;
+            val it : int * int = (5, 6)
+            > (1,2) .+ (2,3) .* (3,7);;
+            val it : int * int = (11, 14)
+    Note that the F# syntax for defining such an infix operator looks like this:
+      let (.+) (a,b) (c,d) = ...
+    Also note that .+ and .* get the same precedences as + and *, respectively, which is why the second example above 
+    gives the result it does.
+    Finally, note that your functions should always return fractions in lowest terms. To implement this, you will need 
+    an auxiliary function to calculate the gcd (greatest common divisor) of the numerator and the denominator; this can 
+    be done very efficiently using Euclid's algorithm, which can be implemented in F# as follows:
+
+      let rec gcd = function
+            | (a,0) -> a
+            | (a,b) -> gcd (b, a % b)
+*)
+
+let rec gcd = function
+        | (a,0) -> a
+        | (a,b) -> gcd (b, a % b)
+
+let (.+) (a, b) (c, d) = 
+    a + c
+
+(1,2) .+ (1,3);;
+
+(*
 17. Write an F# function revlists xs that takes a list of lists xs and reverses all the sub-lists:
   > revlists [[0;1;1];[3;2];[];[5]];;
         val it : int list list = [[1; 1; 0]; [2; 3]; []; [5]]
