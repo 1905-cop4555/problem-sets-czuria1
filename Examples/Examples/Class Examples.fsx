2794b1589adf079186a73159a52e2eed286fff0e
czuria1 <czuria1@gmail.com>
2019-05-21 11:59:40 -0400

    Trying out in class examples on discriminated and simple unions

--- Examples/Examples/Class Examples.fsx
+++ Examples/Examples/Class Examples.fsx
@@ -5,3 +5,21 @@ type Person = {Name:string; Age:int}
 let p = {Age = 15; Name = "David"}
 
 p.Age
+
+// discriminated unions
+type Shape = 
+    | Rectangle of width : float * length : float
+    | Circle of radiius : float
+    | Prism of width : float * float * height : float
+
+// constructing unions
+let rect1 = Rectangle(length  = 1.3, width = 10.0)
+let rect2 = Rectangle(width = 10.0, length  = 1.3)  // order does not matter
+
+// simple unions
+type color = Red | Green | Blue // constructors
+
+let opinion = function 
+    //| Red -> "nice" // without this you get a warning
+    | Blue -> "lovely"
+    | Green -> "ugly"
