a16a455ac6d2b98198d4736f11757a315b63d8bb
czuria1 <czuria1@gmail.com>
2019-05-22 18:23:48 -0400

    First attempt at a record example

---
+++ Problem-Sets-2/Problem-Sets-2/Problem Set 2.fsx
@@ -0,0 +1,11 @@
+﻿(*
+11. Record
+    Create a record type for Name, Credits and GPA.
+    Create a record instance with the values "Jones", 109, 3.85.
+*)
+
+// record type
+type Student = {Name: string; Credits: int; GPA: float}
+
+// record instance
+let s = {Name = "Jones"; Credits = 109; GPA = 3.85}
