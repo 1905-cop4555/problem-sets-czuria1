5e8bbfd3af0618c738bd590632b1725646529f5c
czuria1 <czuria1@gmail.com>
2019-05-24 22:09:33 -0400

    Added Problem 3 to problem sets

--- Problem-Sets-2/Problem-Sets-2/Problem Set 2.fsx
+++ Problem-Sets-2/Problem-Sets-2/Problem Set 2.fsx
@@ -17,6 +17,38 @@ let coor1 = Tuple(x = 1, y = 2)
 let coor2 = Threeple(x = 1, y = 2, z = 3)
 let coor3 = Fourple(x = 1, y = 2, z = 3, w = 4)
 
+let coord_operator = function
+    | Tuple -> "Tuple"
+
+
+(*
+3. In the Notes on Programming Language Syntax page, an example syntax checker for a simple language is given, using C syntax. Write the syntax checker using F#, but you may only use functional programming and immutable date.
+    Create the list of tokens as a discriminated union, which (in the simplest case) looks like an enumeration.
+
+    type TERMINAL = IF|THEN|ELSE|BEGIN|END|PRINT|SEMICOLON|ID|EOF
+    With this type declared, you can use the terminals like you would use enumerated values in Java.
+    Use immutable data. The C-code example uses mutable data. Pass the program into the start symbol function. Pass the input yet to be processed to each non-terminal function.
+
+    The main function might look like this:
+
+        let test_program program =
+          let result = program |> S
+          match result with 
+          | [] -> failwith "Early termination or missing EOF"
+          | x::xs -> if x = EOF then accept() else error()
+    You do not have to tokenize the input strings. Assume that the tokenizing has been done. Pass a list of tokens that represent a program into the start symbol. Try these program examples:
+
+    [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;END;ELSE;PRINT;ID;EOF]
+
+    [IF;ID;THEN;IF;ID;THEN;PRINT;ID;ELSE;PRINT;ID;ELSE;BEGIN;PRINT;ID;END;EOF]
+          
+    Causes error: 
+    [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;SEMICOLON;END;ELSE;PRINT;ID;EOF]
+    Print an accept message when the input is valid and completely consumed. Generate appropriate error messages for incorrect symbols, not enough input, and too much input.
+*)
+
+type TERMINAL = IF|THEN|ELSE|BEGIN|END|PRINT|SEMICOLON|ID|EOF
+
 (*
 11. Record
     Create a record type for Name, Credits and GPA.
