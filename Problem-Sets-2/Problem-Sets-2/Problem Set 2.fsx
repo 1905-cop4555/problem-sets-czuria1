f9965efebc414d4a4c7fde33c133d362a859b27b
czuria1 <czuria1@gmail.com>
2019-05-27 13:36:41 -0400

    Added problem 1

--- Problem-Sets-2/Problem-Sets-2/Problem Set 2.fsx
+++ Problem-Sets-2/Problem-Sets-2/Problem Set 2.fsx
@@ -1,4 +1,25 @@
 ﻿(*
+1. Recall that an F# function that takes two arguments can be coded in either uncurried form (in which case it takes a 
+pair as its input) or curried form (in which case it takes the first argument and returns a function that takes the 
+second argument). In fact it is easy to convert from one form to the other in F#. To this end, define an F# function 
+curry f that converts an uncurried function to a curried function, and an F# function uncurry f that does the opposite 
+conversion. For example,
+      > (+);;
+        val it : (int -> int -> int) = <fun:it@13-7>
+        > let plus = uncurry (+);;
+        val plus : (int * int -> int)
+        > plus (2,3);;
+        val it : int = 5
+        > let cplus = curry plus;;
+        val cplus : (int -> int -> int)
+        > let plus3 = cplus 3;;
+        val plus3 : (int -> int)
+        > plus3 10;;
+        val it : int = 13
+    What are the types of curry and uncurry?
+    *)
+
+(*
 2. Discriminated Union
     a. Create a discriminated union for Coordinates that can be a Tuple, Threeple or Fourple that represent tuples of size two, three and four. The type for the union should be polymorphic.
     b. Instantiate a Tuple of integers, a Threeple of floats and a Fourple of strings.
