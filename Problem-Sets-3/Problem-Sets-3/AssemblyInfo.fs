be10d8fe30e038798b25eec1fd091459bba80cc8
czuria1 <czuria1@gmail.com>
2019-05-30 21:27:04 -0400

    Add problem sets 3 folder

---
+++ Problem-Sets-3/Problem-Sets-3/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace ProblemSets3
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Problem-Sets-3")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
