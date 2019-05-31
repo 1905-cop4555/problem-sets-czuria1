679e3d4d31960a3f2b39b93741c6c0c8441fced6
czuria1 <czuria1@gmail.com>
2019-05-31 17:15:03 -0400

    Added problem 2 from problem set

--- Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
+++ Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
@@ -6,3 +6,13 @@
 type tree = 
     | Empty 
     | Cons of head:int * tail:tree
+
+(*
+2. This CFG recognizes some strings of zeros and ones.
+        S → 0A | 1B
+        A → 0AA | 1S | 1
+        B → 1BB | 0S | 0 
+    a. Describe the strings that the CFG recognizes.
+    b. This language is ambiguous. Find a string that is recognized by this grammar which has two derivations.
+    c. Show the two derivation trees for the string in part (b).
+*)
