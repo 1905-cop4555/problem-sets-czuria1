629ed79f11d720c9d09f935bf454c25a93c4f189
czuria1 <czuria1@gmail.com>
2019-05-30 21:34:22 -0400

    Added problem 1 from the problem set

--- Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
+++ Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
@@ -1,1 +1,5 @@
-﻿
+﻿(*
+1. Building a simple tree.
+    Create a discriminated union that can represent a linked list of integers.
+    Write a function that converts a list into a linked list of nodes.
+*)
