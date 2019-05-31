43246824b69592527ba97582f2f0a3560ec25af1
czuria1 <czuria1@gmail.com>
2019-05-31 17:13:37 -0400

    First attempt at creating discriminated union for a linked list

--- Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
+++ Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
@@ -1,5 +1,8 @@
 ﻿(*
 1. Building a simple tree.
-    Create a discriminated union that can represent a linked list of integers.
-    Write a function that converts a list into a linked list of nodes.
+    a. Create a discriminated union that can represent a linked list of integers.
+    b. Write a function that converts a list into a linked list of nodes.
 *)
+type 'a tree = 
+    | Empty 
+    | Cons of head:'a * tail:'a tree
