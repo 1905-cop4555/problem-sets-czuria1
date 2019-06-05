9d506c39d2654a4bb1476113c5e5816127fc2cf9
czuria1 <czuria1@gmail.com>
2019-06-05 15:08:37 -0400

    Added problem 5 from problem set

--- Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
+++ Problem-Sets-3/Problem-Sets-3/Problem Set 3.fsx
@@ -20,3 +20,15 @@ let rec convert = function
     b. This language is ambiguous. Find a string that is recognized by this grammar which has two derivations.
     c. Show the two derivation trees for the string in part (b).
 *)
+
+(*
+5. Write a tail-recursive F# function interleave(xs,ys) that interleaves two lists:
+      > interleave ([1;2;3],[4;5;6]);;
+                val it : int list = [1; 4; 2; 5; 3; 6] 
+    Assume that the two lists have the same length.
+
+    Compare the timing of the recursive function from Problem Set 1 with this tail-recursive version. Time these examples in both versions.
+
+    list1 = [1..2..19999], list2 = [2..2..20000]
+    list1 = [1..2..199999], list2 = [2..2..200000]
+*)
