79da932b59cf095e431142ae84394f4981b54aee
czuria1 <czuria1@gmail.com>
2019-06-11 12:04:50 -0400

    Added one more base case for pred

---
+++ Problem-Sets-3/Problem-Sets-3/pcf.fsx
@@ -0,0 +1,33 @@
+﻿module Interp_script
+
+// Skeleton file for PCF interpreter
+
+// This sets F# to read from whatever directory contains this source file.
+System.Environment.set_CurrentDirectory __SOURCE_DIRECTORY__;;
+
+#load "parser.fsx"
+
+// This lets us refer to "Parser.Parse.parsefile" simply as "parsefile",
+// and to constructors like "Parser.Parse.APP" simply as "APP".
+open Parser_script.Parse
+
+// Here I show you a little bit of the implementation of interp. Note how ERRORs
+// are propagated, how rule (6) is implemented, and how stuck evaluations
+// are reported using F#'s sprintf function to create good error messages.
+let rec interp = function
+| APP (e1, e2) ->
+    match (interp e1, interp e2) with
+    | (ERROR s, _)  -> ERROR s        // ERRORs are propagated
+    | (_, ERROR s)  -> ERROR s
+    | (SUCC, NUM n) -> NUM (n+1)      // Rule (6)
+    | (SUCC, v)     -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
+    | (PRED, NUM 0) -> NUM 0
+    | (PRED, NUM n) -> NUM (n-1)
+    | (PRED, v)     -> ERROR (sprintf "'pred' needs int argument, not '%A'" v)
+
+// Here are two convenient abbreviations for using your interpreter.
+let interpfile filename = filename |> parsefile |> interp
+
+let interpstr sourcecode = sourcecode |> parsestr |> interp
+
+let result = interpstr "succ 0"
